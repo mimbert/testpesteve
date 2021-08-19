ced77c628187f141767fc59cb9cb8561d68d8a05
Matthieu Imbert <matthieu.imbert@inria.fr>
2021-08-19 10:37:26 +0200

    tee for output simultaneously to container log and output log

--- client-server-gnuradio-udp-prompt-for-exit.sh
+++ client-server-gnuradio-udp-prompt-for-exit.sh
@@ -3,7 +3,7 @@ echo "$(date): source gnuradio environment"
 source /cortexlab/toolchains/current/bin/cxlb-toolchain-user-conf
 echo "$(date): start gnuradio udp server in prompt for exit mode"
 #./server_prompt_for_exit.py | tee gnuradio-udp-server-prompt-for-exit.log &
-nohup ./server_prompt_for_exit.py > gnuradio-udp-server-prompt-for-exit.log &
+nohup ./server_prompt_for_exit.py | tee gnuradio-udp-server-prompt-for-exit.log &
 echo "$(date): wait a bit"
 sleep 5
 echo "$(date): start gnuradio udp client in prompt for exit mode"
--- client-server-gnuradio-udp-run-to-completion.sh
+++ client-server-gnuradio-udp-run-to-completion.sh
@@ -3,7 +3,7 @@ echo "$(date): source gnuradio environment"
 source /cortexlab/toolchains/current/bin/cxlb-toolchain-user-conf
 echo "$(date): start gnuradio udp server in run to completion mode"
 #./server_run_to_completion.py | tee gnuradio-udp-server-run-to-completion.log &
-nohup ./server_run_to_completion.py > gnuradio-udp-server-run-to-completion.log &
+nohup ./server_run_to_completion.py | tee gnuradio-udp-server-run-to-completion.log &
 echo "$(date): wait a bit"
 sleep 5
 echo "$(date): start gnuradio udp client in run to completion mode"
